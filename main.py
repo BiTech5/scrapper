dc79798134bbc98390ae7e2d17c1b72e7d742292
BiTech5 <bikikc7@gmail.com>
2024-06-24 12:56:44 +0545

    making function

--- main.py
+++ main.py
@@ -8,8 +8,18 @@ soup=BeautifulSoup(r.text,'lxml')
 # print(soup)
 boxes=soup.find_all('div',class_="co-list__itm")
 # print(len(boxes))
-company_name=soup.find_all('p',class_="co-box__name font-medium")
-for i in company_name:
+p_tag=soup.find_all('p',class_="co-box__name font-medium")
+def data(att):
+    att=str(att)
+    url="https://techbehemoths.com"+att
+
+    r=re.get(url)
+    soup=BeautifulSoup(r.text,'lxml')
+    company_name=soup.find('h1',class_="co-box__name font-medium larger")
+    print(company_name.text)
+for i in p_tag:
     a_tag=i.find('a')
     if a_tag:
-        print(i.text)
+        href=a_tag['href']
+        data(href)
+        
