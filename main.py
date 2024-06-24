5d8b88718b95283c507ed63c34c1e5196aa2d007
BiTech5 <bikikc7@gmail.com>
2024-06-24 13:47:27 +0545

    adding condtion

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
 import requests as re
 from bs4 import BeautifulSoup
-url=r"https://techbehemoths.com/companies/kathmandu"
-r=re.get(url)
-soup=BeautifulSoup(r.text,'lxml')
-# print(soup)
-boxes=soup.find_all('div',class_="co-list__itm")
-# print(len(boxes))
-p_tag=soup.find_all('p',class_="co-box__name font-medium")
-
 
 def data(att):
     att=str(att)
@@ -19,18 +11,34 @@ def data(att):
     company_website=soup.find('a',class_="btn relative btn-outlined btn-black btn-special")
     company_website=company_website.find('span','val ellipsis')
     company_nbr=soup.find('a',class_='btn btn-outlined btn-black btn-special relative')
-    company_nbr=company_nbr.find('span','val ellipsis')
+    if company_nbr:
+        company_nbr=company_nbr.find('span','val ellipsis')
+        nbr=company_nbr.text
+    else:
+       nbr=''
     location=soup.find('span',"co-box__loc-itm")
     all_data={
-        'Name':company_name.text,
-        "Company Website":company_website.text,
-        'Company Number':company_nbr.text,
-        'Location':location.text
+        'Name':company_name.text if company_name else '',
+        "Company Website":company_website.text if company_website else '',
+        'Company Number':nbr,
+        'Location':location.text if location else ''
     }
     return all_data
 
-for i in p_tag:
-    a_tag=i.find('a')
-    if a_tag:
-        href=a_tag['href']
-        print(data(href))
+def pagination(url):
+    r=re.get(url)
+    soup=BeautifulSoup(r.text,'lxml')
+    p_tag=soup.find_all('p',class_="co-box__name font-medium")
+    for i in p_tag:
+        a_tag=i.find('a')
+        if a_tag:
+            href=a_tag['href']
+            print(data(href))
+i=0
+while i!=3:
+    i+=1
+    nbr=str(i)
+    url="https://techbehemoths.com/companies/kathmandu?page="+nbr
+    pagination(url)
+    print('Page',i,'Finished')
+
